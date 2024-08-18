a51cddfb30356f2accdbcda16e26cbb2b937684b
Woo Jin Lee <100455176+wjin-lee@users.noreply.github.com>
2024-08-18 18:49:30 +1200

    feat: add landing page. (#2)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'landing', component: LandingComponent },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+];
